121d4ef9d2add45d45f1bbd457bc2a18eb56b65c
xenosign <xenosign@naver.com>
2022-03-10 09:01:13 +0900

    test requestAni

--- src/components/canvas/Canvas.tsx
+++ src/components/canvas/Canvas.tsx
@@ -2,6 +2,8 @@ import React, { useRef, useState, useCallback, useEffect } from "react";
 import { Wave } from "./wave.js";
 import { SurferController } from "./surfer-controlloer.js";
 import { Sun } from "./sun.js";
+import { visitLexicalEnvironment } from "typescript";
+import { timeStamp } from "console";
 
 function Canvas() {
   const canvasRef = React.useRef<HTMLCanvasElement>(null);
@@ -19,21 +21,43 @@ function Canvas() {
       }
     }
 
-    // Draw a rectangle
+    var x = 0;
     const render = () => {
-      if (ctx) {
-        let x = 0;
+      if (ctx && x < 250) {
+        //ctx.clearRect(0, 0, 500, 500);
         ctx.beginPath();
         ctx.arc(x, 150, 10, 0, Math.PI * 2, false);
         ctx.closePath();
         ctx.fill();
         x += 2;
+        console.log("재귀함수 : ", x);
 
-        requestAnimationFrame(render);
+        render();
       }
     };
 
-    requestAnimationFrame(render);
+    var y = 0;
+    var now = 0;
+    var gap = 0;
+    const aniRender = (timestamp: number) => {
+      if (ctx && y < 250) {
+        gap = timestamp - now;
+        //ctx.clearRect(0, 0, 500, 500);
+        ctx.beginPath();
+        ctx.arc(y, 250, 10, 0, Math.PI * 2, false);
+        ctx.closePath();
+        ctx.fill();
+        y += 2;
+        now = timestamp;
+        console.log("timestamp gap : ", gap);
+        // console.log("timestamp : ", timestamp);
+
+        requestAnimationFrame(aniRender);
+      }
+    };
+
+    //render();
+    requestAnimationFrame(aniRender);
   }, [ctx]);
 
   return (
@@ -42,6 +66,7 @@ function Canvas() {
         textAlign: "center",
       }}
     >
+      <div id="test"></div>
       <canvas
         id="canvas"
         ref={canvasRef}
